a7bd1df45caad34ffbbc8fe54e2c6bcb30f49f15
escomputers <emilianos13@gmail.com>
2023-03-26 03:04:53 +0200

    bme280 fixes

--- sensors/bme280/air-temperature.py
+++ sensors/bme280/air-temperature.py
@@ -3,6 +3,10 @@ import requests
 import threading
 import board
 from adafruit_bme280 import basic as adafruit_bme280
+import syslog
+import datetime
+
+syslog.openlog(facility=syslog.LOG_LOCAL0)
 
 LLA_TOKEN = os.environ["TOKEN"]
 
@@ -14,12 +18,18 @@ def sensing():
 
     # GET & SEND AIR TEMPERATURE
     air_temperature = str(("%.2f" % round(bme280.temperature, 2)))
-    requests.post('http://localhost:8123/api/states/sensor.air_temperature', headers={
-        'Authorization': 'Bearer ' + LLA_TOKEN,
-        'Content-Type': 'application/json'
-    }, json={
-        'state': air_temperature,
-        'attributes': {
-            'unit_of_measurement': '°C'
-        }
-    })
+    try:
+        requests.post('http://localhost:8123/api/states/sensor.air_temperature', headers={
+            'Authorization': 'Bearer ' + LLA_TOKEN,
+            'Content-Type': 'application/json'
+        }, json={
+            'state': air_temperature,
+            'attributes': {
+                'unit_of_measurement': '°C'
+            }
+        })
+    except ConnectionError:
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        syslog.syslog(syslog.LOG_WARNING, '[' + timestamp + ']' + '[WARNING HASSIO REST API for BME280-AIRTEMP] Cannot send sensor data to Home Assistant REST endpoint.')
+
+sensing()
