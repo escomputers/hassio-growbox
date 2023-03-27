7eb5bba82a1cb18701e91c21ad55fe0f713b2cce
escomputers <emilianos13@gmail.com>
2023-03-28 00:29:40 +0200

    small refactor

--- sensors/threadsManager.py
+++ sensors/threadsManager.py
@@ -2,17 +2,19 @@ import os
 import threading
 
 
+# List of sensor reading scripts
+scripts = ['sensors/bme280/air-temperature.py', 'sensors/bme280/air-humidity.py', 
+    'sensors/bme280/air-pressure.py', 'sensors/sht20/soil-temperature.py', 
+    'sensors/sht20/soil-humidity.py'
+]
+
+
 def run_sensor_scripts():
     threading.Timer(20.0, run_sensor_scripts).start()
 
-    # List of sensor reading scripts
-    scripts = ['sensors/bme280/air-temperature.py', 'sensors/bme280/air-humidity.py', 
-		'sensors/bme280/air-pressure.py', 'sensors/sht20/soil-temperature.py', 
-		'sensors/sht20/soil-humidity.py'
-	]
-    
     # Loop through each script and run it while holding the lock
     for script in scripts:
         os.system('python ' + script)
+
 
 run_sensor_scripts()
