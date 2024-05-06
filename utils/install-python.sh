635848de71130aded1724cccd5c9a79ec4f33d1e
emilianospada <emilianos13@gmail.com>
2024-05-06 18:16:16 +0200

    Update install-python.sh

--- utils/install-python.sh
+++ utils/install-python.sh
@@ -2,6 +2,9 @@
 
 version=$1
 
+# Extracting major and minor version components
+major_minor_version="${version%.*}"
+
 cd /tmp/
 wget https://www.python.org/ftp/python/"$version"/Python-"$version".tgz
 tar xzf Python-"$version".tgz
@@ -11,3 +14,6 @@ sudo ./configure --prefix=/usr/local/python/"$version"/ --enable-optimizations
 sudo make -j "$(nproc)"
 sudo make altinstall
 sudo rm /tmp/Python-"$version".tgz
+
+# Update sym links
+sudo ln -sf /usr/local/python/"$version"/bin/python"$major_minor_version" /usr/bin/python3
