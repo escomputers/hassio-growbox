6e3bf76848fc843636ecdd3877ee8a1a56ff317f
escomputers <emilianos13@gmail.com>
2024-05-20 12:49:22 +0000

    fixed python install script

--- utils/install-python.sh
+++ utils/install-python.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Install build requirements
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget -y
+
 version=$1
 
 # Extracting major and minor version components
@@ -7,13 +10,22 @@ major_minor_version="${version%.*}"
 
 cd /tmp/
 wget https://www.python.org/ftp/python/"$version"/Python-"$version".tgz
-tar xzf Python-"$version".tgz
+tar -xf Python-"$version".tgz
 cd Python-"$version"
 
-sudo ./configure --prefix=/usr/local/python/"$version"/ --enable-optimizations
-sudo make -j "$(nproc)"
-sudo make altinstall
+./configure --enable-optimizations
+sudo make install
+sudo make altinstall  # set as default version from now on
+
+# Link "python" to the new installation 
+sudo ln -s /usr/local/bin/python3 /usr/bin/python
+
+# Clean up
 sudo rm /tmp/Python-"$version".tgz
 
-# Update sym links
-sudo ln -sf /usr/local/python/"$version"/bin/python"$major_minor_version" /usr/bin/python3
+# Verify installation
+echo "python3 -V output:"
+python3 -V
+
+echo "python -V output:"
+python -V
