66fe804be7906102d6ba1a2fa9fa66ed82bd6906
emilianospada <emilianos13@gmail.com>
2024-05-20 15:43:27 +0200

    Update install-python.sh

--- utils/install-python.sh
+++ utils/install-python.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Install build requirements
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget -y
+
 version=$1
 
 # Extracting major and minor version components
@@ -7,13 +10,12 @@ major_minor_version="${version%.*}"
 
 cd /tmp/
 wget https://www.python.org/ftp/python/"$version"/Python-"$version".tgz
-tar xzf Python-"$version".tgz
+tar -xf Python-"$version".tgz
 cd Python-"$version"
 
-sudo ./configure --prefix=/usr/local/python/"$version"/ --enable-optimizations
-sudo make -j "$(nproc)"
-sudo make altinstall
-sudo rm /tmp/Python-"$version".tgz
+./configure --enable-optimizations
+sudo make install
+sudo make altinstall  # set as default version from now on
 
-# Update sym links
-sudo ln -sf /usr/local/python/"$version"/bin/python"$major_minor_version" /usr/bin/python3
+# Clean up
+sudo rm /tmp/Python-"$version".tgz
