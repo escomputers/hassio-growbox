ac344567caa0c7b02e337e2e776cf8e570f601ea
emilianospada <emilianos13@gmail.com>
2024-05-20 23:21:16 +0200

    Update install-docker-ubuntu.sh

--- utils/install-docker-ubuntu.sh
+++ utils/install-docker-ubuntu.sh
@@ -14,7 +14,3 @@ echo \
 sudo apt update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-
-sudo usermod -aG docker $USER
-
-newgrp docker
